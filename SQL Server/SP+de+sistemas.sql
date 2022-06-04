7df6f54ef197bfcbc14b669528d2a70b2fd654cb
Iara Camargos <84880369+IC12@users.noreply.github.com>
2022-06-03 22:59:03 -0300

    Scripts atts

---
+++ SQL Server/SP+de+sistemas.sql
@@ -0,0 +1,6 @@
+
+EXEC sp_columns @table_name = 'TABELA DE CLIENTES', @table_owner = 'dbo'
+
+EXEC sp_tables @table_name = 'TAB%', @table_owner = 'dbo', @table_qualifier = 'SUCOS_VENDAS'
+
+--Procedimentos armazenados do Sistema
